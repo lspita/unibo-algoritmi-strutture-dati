685b128967184ec26fc6625d8f4ca859e6245553
Ludovico M. Spitaleri <ludovico.spitaleri@gmail.com>
2024-02-29 20:50:27 +0100

    shooting stars

--- Laboratorio/ShootingStars/shooting-stars.c
+++ Laboratorio/ShootingStars/shooting-stars.c
@@ -164,53 +164,218 @@ consentire all'utente di giocare una partita.
 #include <stdlib.h>
 #include <assert.h>
 
-#define STELLA 1
-#define STELLA_SYMBOL '*'
+#define STAR 1
+#define STAR_SYMBOL '*'
 
-#define BUCO 0
-#define BUCO_SYMBOL '.'
+#define BLACK_HOLE 0
+#define BLACK_HOLE_SYMBOL '.'
 
 #define FIELD_DIMENSION 3
+#define CENTER ((int)(FIELD_DIMENSION / 2)) /* floored dimension / 2 */
 
-void print_field(int (*field)[FIELD_DIMENSION])
+#define LOSS 0
+#define WIN 1
+#define CONTINUE 2
+
+typedef struct Cell
+{
+    int row;
+    int col;
+} Cell;
+
+Cell new_cell(int row, int col)
+{
+    Cell c;
+    c.row = row;
+    c.col = col;
+
+    return c;
+}
+
+Cell index_to_cell(int index)
+{
+    return new_cell(index / FIELD_DIMENSION, index % FIELD_DIMENSION);
+}
+
+int cell_to_index(int row, int col)
+{
+    return (row * FIELD_DIMENSION) + col;
+}
+
+void print_field(int (*const field)[FIELD_DIMENSION])
 {
     int i, j;
     for (i = 0; i < FIELD_DIMENSION; i++)
     {
         for (j = 0; j < FIELD_DIMENSION; j++)
         {
-            char symbol = '\0';
             switch (field[i][j])
             {
-            case STELLA:
-                symbol = STELLA_SYMBOL;
+            case STAR:
+                printf("%d", cell_to_index(i, j));
                 break;
-            case BUCO:
-                symbol = BUCO_SYMBOL;
-                break;
-
-            default:
+            case BLACK_HOLE:
+                printf("%c", BLACK_HOLE_SYMBOL);
                 break;
             }
-
-            printf("%c ", symbol);
+            putchar(' ');
         }
         putchar('\n');
     }
+    putchar('\n');
+}
+
+int in_bounds(const Cell *const cell)
+{
+    return (cell->row >= 0 && cell->row < FIELD_DIMENSION &&
+            cell->col >= 0 && cell->col < FIELD_DIMENSION);
+}
+
+Cell read_cell(int (*const field)[FIELD_DIMENSION])
+{
+    int index = -1;
+    Cell cell = {-1, -1};
+
+    do
+    {
+        int i, j;
+
+        printf("Cell to explode [");
+        for (i = 0; i < FIELD_DIMENSION; i++)
+        {
+            for (j = 0; j < FIELD_DIMENSION; j++)
+            {
+                if (field[i][j] == STAR)
+                {
+                    printf("%d, ", cell_to_index(i, j));
+                }
+            }
+        }
+        printf("\b\b]: ");
+
+        scanf("%d", &index);
+        cell = index_to_cell(index);
+    } while (!in_bounds(&cell) || field[cell.row][cell.col] != STAR);
+
+    return cell;
+}
+
+void explode_cell(const Cell *const cell, int (*const field)[FIELD_DIMENSION])
+{
+    switch (field[cell->row][cell->col])
+    {
+    case STAR:
+        field[cell->row][cell->col] = BLACK_HOLE;
+        break;
+    case BLACK_HOLE:
+        field[cell->row][cell->col] = STAR;
+        break;
+    }
+}
+
+int is_angle(const Cell *const cell)
+{
+    return ((cell->row == 0 || cell->row == FIELD_DIMENSION - 1) &&
+            (cell->col == 0 || cell->col == FIELD_DIMENSION - 1));
+}
+
+void explode(const Cell *const cell, int (*field)[FIELD_DIMENSION])
+{
+    int angle;
+    Cell bounds[8];
+
+    bounds[0] = new_cell(cell->row - 1, cell->col);     /* up */
+    bounds[1] = new_cell(cell->row + 1, cell->col);     /* down */
+    bounds[2] = new_cell(cell->row, cell->col - 1);     /* left */
+    bounds[3] = new_cell(cell->row, cell->col + 1);     /* right */
+    bounds[4] = new_cell(cell->row - 1, cell->col - 1); /* nw */
+    bounds[5] = new_cell(cell->row - 1, cell->col + 1); /* ne */
+    bounds[6] = new_cell(cell->row + 1, cell->col - 1); /* sw */
+    bounds[7] = new_cell(cell->row + 1, cell->col + 1); /* se */
+
+    explode_cell(cell, field);
+    angle = is_angle(cell);
+
+    if (angle)
+    {
+        /* explode everithing around in bounds */
+        int i;
+        for (i = 0; i < 8; i++)
+        {
+            const Cell *const current = &(bounds[i]);
+            if (in_bounds(current))
+            {
+                explode_cell(current, field);
+            }
+        }
+    }
+    else
+    {
+        /* explode only if both vertical or both horizontal are in bounds */
+        const Cell *const up_ptr = &(bounds[0]), *const down_ptr = &(bounds[1]);
+        const Cell *const left_ptr = &(bounds[2]), *const right_ptr = &(bounds[3]);
+
+        if (in_bounds(up_ptr) && in_bounds(down_ptr))
+        {
+            explode_cell(up_ptr, field);
+            explode_cell(down_ptr, field);
+        }
+
+        if (in_bounds(left_ptr) && in_bounds(right_ptr))
+        {
+            explode_cell(left_ptr, field);
+            explode_cell(right_ptr, field);
+        }
+    }
+}
+
+int check_status(int (*field)[FIELD_DIMENSION])
+{
+    int count_blackholes = 0;
+
+    int i, j;
+    for (i = 0; i < FIELD_DIMENSION; i++)
+    {
+        for (j = 0; j < FIELD_DIMENSION; j++)
+        {
+            if (field[i][j] == BLACK_HOLE)
+            {
+                count_blackholes++;
+            }
+        }
+    }
+
+    if (count_blackholes == 1 && field[CENTER][CENTER] == BLACK_HOLE)
+    {
+        printf("\nVICTORY\n");
+        return WIN;
+    }
+    if (count_blackholes == FIELD_DIMENSION * FIELD_DIMENSION)
+    {
+        printf("\nYOU LOST\n");
+        return LOSS;
+    }
+
+    return CONTINUE;
 }
 
 int main(void)
 {
     int field[FIELD_DIMENSION][FIELD_DIMENSION] = {
-        {BUCO, BUCO, BUCO},
-        {BUCO, STELLA, BUCO},
-        {BUCO, BUCO, BUCO},
+        {BLACK_HOLE, BLACK_HOLE, BLACK_HOLE},
+        {BLACK_HOLE, STAR, BLACK_HOLE},
+        {BLACK_HOLE, BLACK_HOLE, BLACK_HOLE},
     };
 
-    int run = 1;
-    while (run == 1)
+    int status = CONTINUE;
+    Cell cell;
+
+    while (status == CONTINUE)
     {
         print_field(field);
+        cell = read_cell(field);
+        explode(&cell, field);
+        status = check_status(field);
     }
 
     return EXIT_SUCCESS;
