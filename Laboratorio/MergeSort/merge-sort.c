1288e8905bc3f29bc78f550ab5cfba7add8811b5
Ludovico M. Spitaleri <ludovico.spitaleri@gmail.com>
2024-03-07 18:15:14 +0100

    merge sort funzionante

--- Laboratorio/MergeSort/merge-sort.c
+++ Laboratorio/MergeSort/merge-sort.c
@@ -28,7 +28,8 @@
 ![John von Neumann By LANL -
 <http://www.lanl.gov/history/atomicbomb/images/NeumannL.GIF> (archive
 copy), Attribution,
-<https://commons.wikimedia.org/w/index.php?curid=3429594>](von-neumann.jpg "John von Neumann")
+<https://commons.wikimedia.org/w/index.php?curid=3429594>](von-neumann.jpg "John
+von Neumann")
 
 Implementare l'algoritmo _Merge sort_ in modo tale che i test forniti
 nello schema di programma allegato vengano superati. Merge sort è
@@ -85,7 +86,8 @@ dove:
 
 - `base` è un puntatore all'inizio dell'array da ordinare;
 
-- `nmemb` è il numero di elementi dell'array da ordinare (attenzione, **non** il numero di byte!)
+- `nmemb` è il numero di elementi dell'array da ordinare (attenzione, **non** il
+numero di byte!)
 
 - `size` è la dimensione in byte di ciascuno degli elementi dell'array
 
@@ -219,18 +221,35 @@ specifica è indicata nei commenti al codice.
 
 ***/
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <assert.h>
 
 /* Fonde i sottovettori ordinati `v[p..q]` e `v[q+1..r]`. Usa
    `buffer[]` come array temporaneo. `buffer[]` ha la stessa lunghezza
    dell'intero array `v[]`. */
-void merge(int *v, int p, int q, int r, int *buffer)
-{
-    /* [TODO] */
+void merge(int *v, int p, int q, int r, int *buffer) {
+  int i = p, j = q + 1, current = p;
+  int stop_i = 0, stop_j = 0;
+  while (!stop_i || !stop_j) {
+    stop_i = i > q;
+    stop_j = j > r;
+
+    if (!stop_i && (stop_j || v[i] <= v[j])) {
+      buffer[current] = v[i];
+      i++;
+    } else if (!stop_j && (stop_i || v[j] <= v[i])) {
+      buffer[current] = v[j];
+      j++;
+    }
+
+    current++;
+  }
+  for (i = p; i <= r; i++) {
+    v[i] = buffer[i];
+  }
 }
 
 /* Ordina il sottovettore buffer[p..r] (estremi inclusi) usando
@@ -239,9 +258,13 @@ void merge(int *v, int p, int q, int r, int *buffer)
    può essere utilizzato come buffer temporaneo per le operazioni
    `merge()`, senza necessità di allocare un nuovo buffer ad ogni
    chiamata. */
-void merge_sort(int *v, int p, int r, int *buffer)
-{
-    /* [TODO] */
+void merge_sort(int *v, int p, int r, int *buffer) {
+  if (p < r) {
+    int q = (int)((p + r) / 2);
+    merge_sort(v, p, q, buffer);
+    merge_sort(v, q + 1, r, buffer);
+    merge(v, p, q, r, buffer);
+  }
 }
 
 /* Ordina l'array v[] di lunghezza n>=0 usando Merge-Sort. L'utente
@@ -251,30 +274,25 @@ void merge_sort(int *v, int p, int r, int *buffer)
    la necessità di passare gli indici iniziali e finali, e il buffer
    temporaneo. Questa tecnica prende il nome di "funzione
    trampolino". */
-void sort(int *v, int n)
-{
-    int *buffer = (int*)malloc(n * sizeof(*buffer));
-    assert(buffer != NULL); /* evita un warning con VS */
-    merge_sort(v, 0, n-1, buffer);
-    free(buffer);
+void sort(int *v, int n) {
+  int *buffer = (int *)malloc(n * sizeof(*buffer));
+  assert(buffer != NULL); /* evita un warning con VS */
+  merge_sort(v, 0, n - 1, buffer);
+  free(buffer);
 }
 
-void print_array(const int *v, int n)
-{
-    int i;
+void print_array(const int *v, int n) {
+  int i;
 
-    printf("[ ");
-    for (i = 0; i < n; i++) {
-        printf("%d ", v[i]);
-    }
-    printf("]");
+  printf("[ ");
+  for (i = 0; i < n; i++) {
+    printf("%d ", v[i]);
+  }
+  printf("]");
 }
 
 /* Restituisce un valore casuale compreso tra a e b (estremi inclusi) */
-int randab(int a, int b)
-{
-    return a + rand() % (b-a+1);
-}
+int randab(int a, int b) { return a + rand() % (b - a + 1); }
 
 /* Permuta il contenuto dell'array v[] in modo casuale. Per fare
    questo:
@@ -292,92 +310,87 @@ int randab(int a, int b)
    È possibile dimostrare che questo procedimento, applicato fino alla
    fine, produce una permutazione casuale (random shuffle) di v[].
 */
-void random_shuffle(int *v, int n)
-{
-    int i;
+void random_shuffle(int *v, int n) {
+  int i;
 
-    for (i=0; i<n-1; i++) {
-        const int j = randab(i, n-1);
-        const int tmp = v[i];
-        v[i] = v[j];
-        v[j] = tmp;
-    }
+  for (i = 0; i < n - 1; i++) {
+    const int j = randab(i, n - 1);
+    const int tmp = v[i];
+    v[i] = v[j];
+    v[j] = tmp;
+  }
 }
 
 /* Restituisce un intero < 0 se *p1 è minore di *p2 (interpretati come
    interi), 0 se sono uguali, > 0 se il primo è maggiore del
    secondo. */
-int compare(const void *p1, const void *p2)
-{
-    const int v1 = *(const int*)p1;
-    const int v2 = *(const int*)p2;
-    if (v1 < v2)
-        return -1;
-    else if (v1 > v2)
-        return 1;
-    else
-        return 0;
+int compare(const void *p1, const void *p2) {
+  const int v1 = *(const int *)p1;
+  const int v2 = *(const int *)p2;
+  if (v1 < v2)
+    return -1;
+  else if (v1 > v2)
+    return 1;
+  else
+    return 0;
 }
 
 /* Confronta il contenuto di due array v1 e v2 di lunghezza n;
    restituisce l'indice del primo elemento il cui valore differisce
    nei due array, oppure -1 se gli array hanno lo stesso contenuto */
-int compare_vec(const int *v1, const int *v2, int n)
-{
-    int i;
-    for (i=0; i<n; i++) {
-        if (v1[i] != v2[i])
-            return i;
-    }
-    return -1;
+int compare_vec(const int *v1, const int *v2, int n) {
+  int i;
+  for (i = 0; i < n; i++) {
+    if (v1[i] != v2[i])
+      return i;
+  }
+  return -1;
 }
 
 /* Ordina l'array v[] di lunghezza n. Confrontiamo il risultato
    dell'ordinamento con quello prodotto dalla funzione qsort() della
    libreria standard C. Restituisce true (nonzero) se il test ha
    successo, 0 altrimenti. */
-int test(int *v, int n)
-{
-    int result;
-    int *tmp = (int*)malloc(n * sizeof(*tmp));
-    clock_t tstart, elapsed;
-    int diff;
+int test(int *v, int n) {
+  int result;
+  int *tmp = (int *)malloc(n * sizeof(*tmp));
+  clock_t tstart, elapsed;
+  int diff;
 
-    assert(tmp != NULL); /* evita un warning con VS */
-    memcpy(tmp, v, n*sizeof(*v));
-    qsort(tmp, n, sizeof(*tmp), compare);
-    tstart = clock();
-    sort(v, n);
-    elapsed = clock() - tstart;
-    diff = compare_vec(v, tmp, n);
-    if (diff < 0) {
-        printf("Test OK (%f seconds)\n", ((double)elapsed) / CLOCKS_PER_SEC);
-        result = 1;
-    } else {
-        printf("Test FALLITO: v[%d]=%d, atteso=%d\n", diff, v[diff], tmp[diff]);
-        result = 0;
-    }
-    free(tmp);
-    return result;
+  assert(tmp != NULL); /* evita un warning con VS */
+  memcpy(tmp, v, n * sizeof(*v));
+  qsort(tmp, n, sizeof(*tmp), compare);
+  tstart = clock();
+  sort(v, n);
+  elapsed = clock() - tstart;
+  diff = compare_vec(v, tmp, n);
+  if (diff < 0) {
+    printf("Test OK (%f seconds)\n", ((double)elapsed) / CLOCKS_PER_SEC);
+    result = 1;
+  } else {
+    printf("Test FALLITO: v[%d]=%d, atteso=%d\n", diff, v[diff], tmp[diff]);
+    result = 0;
+  }
+  free(tmp);
+  return result;
 }
 
 /* ATTENZIONE: questa macro produce il valore corretto SOLO se v[] è
    un array dichiarato sullo stack (quindi NON con malloc()). La
    macro DEVE essere chiamata all'interno di un blocco in cui è stato
    dichiarato v[] */
-#define ARRAY_LEN(v) (sizeof(v)/sizeof(v[0]))
+#define ARRAY_LEN(v) (sizeof(v) / sizeof(v[0]))
 
-int main( void )
-{
-    int v1[] = {0, 8, 1, 7, 2, 6, 3, 5, 4};
-    int v2[] = {0, 1, 0, 6, 10, 10, 0, 0, 1, 2, 5, 10, 9, 6, 2, 3, 3, 1, 7};
-    int v3[] = {-1, -3, -2};
-    int v4[] = {2, 2, 2};
+int main(void) {
+  int v1[] = {0, 8, 1, 7, 2, 6, 3, 5, 4};
+  int v2[] = {0, 1, 0, 6, 10, 10, 0, 0, 1, 2, 5, 10, 9, 6, 2, 3, 3, 1, 7};
+  int v3[] = {-1, -3, -2};
+  int v4[] = {2, 2, 2};
 
-    test(v1, ARRAY_LEN(v1));
-    test(v2, ARRAY_LEN(v2));
-    test(v3, ARRAY_LEN(v3));
-    test(v4, ARRAY_LEN(v4));
+  test(v1, ARRAY_LEN(v1));
+  test(v2, ARRAY_LEN(v2));
+  test(v3, ARRAY_LEN(v3));
+  test(v4, ARRAY_LEN(v4));
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
