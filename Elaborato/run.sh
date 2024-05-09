8a815a888908eb6c58b4abdf53e974553f215899
Ludovico M. Spitaleri <ludovico.spitaleri@gmail.com>
2024-05-09 16:11:20 +0200

    working test

--- Elaborato/run.sh
+++ Elaborato/run.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
+
 MAINFILE="0001114169"
 COMPILE="gcc -std=c90 -Wall -Wpedantic src/${MAINFILE}.c -o ${MAINFILE}"
-TEST_START=0
-TEST_END=5
+
+TEST_START=$1
+TEST_END=$2
+
+printf "TESTS: $TEST_START to $TEST_END\n\n"
 
 eval "${COMPILE}"
 
 for i in $(seq $TEST_START $TEST_END); do
-  echo "\n-----------------------\nTEST ${i}\n-----------------------"
+  printf "\n-----------------------\nTEST ${i}\n-----------------------\n"
   eval "./${MAINFILE} test${i}.in"
   read _
 done
