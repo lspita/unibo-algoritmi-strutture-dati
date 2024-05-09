7b11c5273e875180aa2c0307b4c0d1ebd3eaf19a
Ludovico M. Spitaleri <ludovico.spitaleri@gmail.com>
2024-05-09 18:18:26 +0200

    test and src folder

--- Elaborato/run.sh
+++ Elaborato/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+SRC_PATH="src/"
+TESTS_PATH="test/"
+
 MAINFILE="0001114169"
-COMPILE="gcc -std=c90 -Wall -Wpedantic src/${MAINFILE}.c -o ${MAINFILE}"
+COMPILE="gcc -std=c90 -Wall -Wpedantic ${SRC_PATH}/${MAINFILE}.c -o ${MAINFILE}"
 
 TEST_START=$1
 TEST_END=$2
@@ -12,6 +15,6 @@ eval "${COMPILE}"
 
 for i in $(seq $TEST_START $TEST_END); do
   printf "\n-----------------------\nTEST ${i}\n-----------------------\n"
-  eval "./${MAINFILE} test${i}.in"
+  eval "./${MAINFILE} ${TESTS_PATH}test${i}.in"
   read _
 done
